857bcc95cc7de80aa76b3b4191f79c9af2f8c2af
Alexander Nikolaev <alexnxg@gmail.com>
2026-04-03 01:10:56 +0300

    Add route entries for post coponents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { PostListComponent } from './features/posts/post-list/post-list';
 import { HomeComponent } from './features/home/home';
 import { NotFoundComponent } from './features/not-found/not-found';
+import { PostDetailsComponent } from './features/posts/post-details/post-details';
+import { PostFormComponent } from './features/posts/post-form/post-form';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 
   { path: 'home', component: HomeComponent },
   { path: 'posts', component: PostListComponent },
-
+  { path: 'posts/new', component: PostFormComponent },
+  { path: 'posts/edit/:id', component: PostFormComponent },
+  { path: 'posts/:id', component: PostDetailsComponent },
   { path: '**', component: NotFoundComponent },
 ];
