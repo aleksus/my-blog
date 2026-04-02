1ef8fab71776c837f71160b3cf4655fea5afb067
Alexander Nikolaev <alexnxg@gmail.com>
2026-04-02 23:00:26 +0300

    Add not-found container

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './features/posts/post-list/post-list';
+import { HomeComponent } from './features/home/home';
+import { NotFoundComponent } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'posts', component: PostListComponent },
+
+  { path: '**', component: NotFoundComponent },
+];
