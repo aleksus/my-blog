18140c85bc1343c4bc6dc984c62e93f88c0520e8
Alexander Nikolaev <alexnxg@gmail.com>
2026-04-09 21:42:35 +0300

    Fix typo

--- src/app/core/constants/api.constants.ts
+++ src/app/core/constants/api.constants.ts
@@ -1,10 +1,10 @@
 export const API_HOST = 'http://localhost:8080';
 
-export const API_BASE_URL = '${API_HOST}/api';
+export const API_BASE_URL = `${API_HOST}/api`;
 
 export const API_ENDPOINTS = {
   users: `${API_BASE_URL}/users`,
   posts: `${API_BASE_URL}/posts`,
   comments: `${API_BASE_URL}/comments`,
-  uploads: `${API_BASE_URL}/uploads`
+  uploads: `${API_BASE_URL}/uploads`,
 };
